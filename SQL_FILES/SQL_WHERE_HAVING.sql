223260c85274d816f96b3a7e76a431368674d33e
fischtank44 <38988674+fischtank44@users.noreply.github.com>
2019-01-09 12:54:39 -0800

    Update SQL_WHERE_HAVING.sql

--- SQL_FILES/SQL_WHERE_HAVING.sql
+++ SQL_FILES/SQL_WHERE_HAVING.sql
@@ -1,3 +1,5 @@
+-- Sample check to see how many of the first 10 units have htbleed values above 392
+
 select unit, round(avg(htbleed_enthalpy):: numeric, 2) as avg_htbleed
 from engine_test_data_train01
 where unit <= 10
