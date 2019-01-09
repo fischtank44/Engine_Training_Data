cdd89be6718678589a5fa82184febe87ac26e95d
fischtank44 <38988674+fischtank44@users.noreply.github.com>
2019-01-09 12:53:44 -0800

    Update CASE_find_last_10.sql

--- SQL_FILES/CASE_find_last_10.sql
+++ SQL_FILES/CASE_find_last_10.sql
@@ -1,3 +1,7 @@
+-- adds column that looks for and flags last 10 cycles with
+-- a boolian flag 1 = last 10
+-- 0 = greater than 10 cycles
+
 select *, case 
           when cycles_to_fail >= 10 
           then 0 
